81e116e6f130c677cd15321fa877b933c4600fb6
Ana Clara <acsn4@aluno.ifal.edu.br>
2021-08-20 15:56:04 -0300

    Inserindo container

--- lib/infos.dart
+++ lib/infos.dart
@@ -43,9 +43,9 @@ class _CadsPageState extends State<InfosPage> {
     return Material(
       child: Container(
         padding: EdgeInsets.only(
-          top: 100,
-          left: 100,
-          right: 100,
+          top: 50,
+          left: 50,
+          right: 50,
         ),
         color: Colors.white,
         child: SizedBox(
@@ -54,15 +54,64 @@ class _CadsPageState extends State<InfosPage> {
           child: Padding(
             padding: const EdgeInsets.all(8.0),
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
                 SizedBox(
-                  width: 300,
-                  height: 300,
+                  width: 400,
+                  height: 150,
                   child: Image.asset('assets/imagens/ampola.jpeg'),
                 ),
-                Text(
-                  'Acompanhe a Rede social',
+                Divider(),
+                Container(
+                  width: 450,
+                  height: 70,
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                    colors: [Color(0xFFDA97FF), Color(0xFF138AB6)],
+                  )),
+                  child: Text(
+                    "Esse aplicativo tem como objetivo garantir segurança e organização na hora de receber a sua vacina!\n Lembre-se: Vacinas Salvam Vidas!",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                        fontSize: 15),
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+                Divider(),
+                Container(
+                  width: 450,
+                  height: 70,
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                    colors: [Color(0xFFDA97FF), Color(0xFF138AB6)],
+                  )),
+                  child: Text(
+                    "Documentação necessária: \n -Comprovante de Residência; \n -Identidade; \n -Cartão do SUS.",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                        fontSize: 15),
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+                Divider(),
+                Container(
+                  width: 450,
+                  height: 40,
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                    colors: [Color(0xFFDA97FF), Color(0xFF138AB6)],
+                  )),
+                  child: Text(
+                    "Perfil do Instagram para mais informações: \n prefeituradearapiraca",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                        fontSize: 15),
+                    textAlign: TextAlign.left,
+                  ),
                 ),
               ],
             ),
